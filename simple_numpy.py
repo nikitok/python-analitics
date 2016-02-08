a2f96c9cc24f2887c9e49a88fc9c91959b1986cf
Nikita Navalikhin <noviiden@gmail.com>
2016-02-08 11:20:13 +0300

    -- example

--- simple_numpy.py
+++ simple_numpy.py
@@ -10,6 +10,8 @@ X = np.random.normal(loc=1, scale=10, size=(50, 50))
 y = X[...,0]
 print y
 
+for x in range(10):
+    print x
 
 #r = np.sum(X, axis = 1)
 #print np.nonzero(r > 10)
--- svm/test2.py
+++ svm/test2.py
@@ -2,18 +2,20 @@
 import numpy as np
 from sklearn.cross_validation import KFold
 from sklearn.cross_validation import cross_val_score
-from sklearn.datasets import fetch_20newsgroups
+#from sklearn.datasets import fetch_20newsgroups
+from sklearn import datasets
+from sklearn.svm import SVC
+from sklearn.grid_search import GridSearchCV
 
-newsgroups = fetch_20newsgroups(
+newsgroups = datasets.fetch_20newsgroups(
         subset='all',
         categories=['alt.atheism', 'sci.space']
 )
 
 X = newsgroups.data
 y = newsgroups.target
-
 #Одна из сложностей работы с текстовыми данными состоит в том, что для них нужно построить числовое представление.
-# Одним из способов нахождения такого представления является вычисление TF-IDF. В Scikit-Learn это реализовано
+#Одним из способов нахождения такого представления является вычисление TF-IDF. В Scikit-Learn это реализовано
 #в классе sklearn.feature_extraction.text.TfidfVectorizer.
 #Преобразование обучающей выборки нужно делать с помощью функции fit_transform, тестовой — с помощью transform.
 
@@ -21,6 +23,30 @@ y = newsgroups.target
 from  sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(analyzer = 'word')
 X = vectorizer.fit_transform(X)
-X
-print X
-print vectorizer.get_feature_names()[7135]
+
+#print X
+#print vectorizer.get_feature_names()[7135]
+
+grid = {'C': np.power(10.0, np.arange(-5, 6))}
+cv = KFold(y.size, n_folds=5, shuffle=True, random_state=241)
+clf = SVC(kernel='linear', random_state=241)
+gs = GridSearchCV(clf, grid, scoring='accuracy', cv=cv)
+gs.fit(X, y)
+for a in gs.grid_scores_:
+    mean = a.mean_validation_score #— оценка качества по кросс-валидации
+    parameters = a.parameters #— значения параметров
+    best_params_ = a.best_params_ #— значения параметров
+    print mean
+    print parameters
+    print best_params_
+
+clf = SVC(kernel='linear', random_state=241, C=10.0)
+clf.fit(X, y)
+resNumber=np.argsort(np.abs(clf.coef_.data))[-10:]
+#print resNumber
+
+
+for i in range(10):
+    print vectorizer.get_feature_names()[resNumber[i]]
+
+print 'end'
